7f0fa1a65e31992711784b81462653d928858e59
Aditya <adityasahoo652@gmail.com>
2025-04-24 14:54:07 +0530

    feat: create RootLayout component with global styles and metadata for EchoPersona

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "react-hot-toast";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "EchoPersona",
+  description:
+    "EchoPersona is an intelligent assistant that mimics your communication style, manages messages when you're away, and handles tasks autonomously.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
